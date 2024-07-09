1aeb75832c123f2d651b42bf011bd2b54dfd6e03
khanxmetu <khanxbahria@gmail.com>
2024-07-09 10:18:11 +0300

    load config from env

--- api_poller/main.py
+++ api_poller/main.py
@@ -1,43 +1,56 @@
+import os
 import time
+import json
 
 import requests
 import pika
 
 class Config:
-    BASE_URL = "https://ws-public.interpol.int"
-    RED_LIST_URL = f"{BASE_URL}/notices/v1/red"
-    HEADERS = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "accept-language": "en-US,en;q=0.9",
-        "cache-control": "no-cache",
-        "pragma": "no-cache",
-        "priority": "u=0, i",
-        "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-    }
-    RESULT_PER_PAGE = 160
-    MAX_PAGES = 1
-    API_RATE_LIMIT_DELAY_MS = 0
+    def __init__(self):
+        self.BASE_URL = "https://ws-public.interpol.int"
+        self.RED_LIST_PATH = "/notices/v1/red"
+        self.HEADERS_FILE_PATH = "headers.json"
+        self.RESULT_PER_PAGE = 160
+        self.MAX_PAGES = 1
+        self.API_RATE_LIMIT_DELAY_MS = 0
+
+    @classmethod
+    def load_from_env(cls):
+        config = cls()
+        config.BASE_URL = os.getenv("BASE_URL")
+        config.RED_LIST_PATH = os.getenv("RED_LIST_PATH")
+        config.RESULT_PER_PAGE = int(os.getenv("RESULT_PER_PAGE"))
+        config.MAX_PAGES = int(os.getenv("MAX_PAGES"))
+        config.API_RATE_LIMIT_DELAY_MS = int(os.getenv("API_RATE_LIMIT_DELAY_MS"))
+        return config
+
 
 class BaseFetcher:
     def __init__(self, config):
         self._config = config
+        self._red_list_url =  f"{self._config.BASE_URL}"
+        self._red_list_url += f"/{self._config.RED_LIST_PATH}"
+        self._headers = {}
+        self._load_headers_from_file(self._config.HEADERS_FILE_PATH)
+
+    def _load_headers_from_file(self, file_path):
+        with open(file_path) as f:
+            data = json.load(f)
+        self._set_headers(data)
+
+    def _set_headers(self, headers):
+        self._headers = headers
 
     def fetch(self):
         pass
 
-    def _send_request(self, url, params={}):
-        r = requests.get(url, headers=Config.HEADERS, params=params)
+    def _send_request_with_custom_headers(self, url, params={}, headers={}):
+        r = requests.get(url, headers=headers, params=params)
         time.sleep(self._config.API_RATE_LIMIT_DELAY_MS // 1000)
-        print(r.text)
+
+    def _send_request(self, url, params={}):
+        r = requests.get(url, headers=self._headers, params=params)
+        time.sleep(self._config.API_RATE_LIMIT_DELAY_MS // 1000)
         return r.json()
 
 class NoticeListFetcher(BaseFetcher):
@@ -58,7 +71,7 @@ class NoticeListFetcher(BaseFetcher):
                     'page': page_no,
                     'resultPerPage': self._config.RESULT_PER_PAGE
                 }
-        data = self._send_request(self._config.RED_LIST_URL, params)
+        data = self._send_request(self._red_list_url, params)
         ids = self._extract_ids_from_page(data)
         return ids
 
@@ -74,7 +87,7 @@ class NoticeDetailFetcher(BaseFetcher):
         super().__init__(config)
 
     def fetch(self, notice_id):
-        url = f"{self._config.RED_LIST_URL}/{notice_id}"
+        url = f"{self._red_list_url}/{notice_id}"
         data = self._send_request(url)
         return data
 
@@ -92,7 +105,7 @@ class FetchWorker:
             print(data)
 
 def main():
-    config = Config()
+    config = Config.load_from_env()
     worker = FetchWorker(config)
     worker.run()
     print("Ran successfully")
