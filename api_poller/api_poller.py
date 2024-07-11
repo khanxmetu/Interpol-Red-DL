04b7dd7211855459345fda26584f3788f0b04d04
khanxmetu <khanxbahria@gmail.com>
2024-07-11 17:28:56 +0300

    exception handling

--- api_poller/api_poller.py
+++ api_poller/api_poller.py
@@ -2,6 +2,7 @@ import os
 import time
 
 from fetch_worker import run_fetch_worker
+from exceptions import APIPollerException
 
 HOUR_TO_MS = 1*60*60_000
 
@@ -10,7 +11,10 @@ def main():
     poll_interval = poll_interval_ms / 1000
     while True:
         print("[!] Worker process started")
-        run_fetch_worker()
+        try:
+            run_fetch_worker()
+        except APIPollerException as e:
+            print(e)
         print("[!] Worker process completed")
         print("[!] waiting for the next run...")
         time.sleep(poll_interval)
