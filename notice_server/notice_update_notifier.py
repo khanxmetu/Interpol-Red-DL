427415529f318fc7ff5f99a27a304808d0337d29
khanxmetu <khanxbahria@gmail.com>
2024-07-12 15:44:30 +0300

    implement NoticeUpdateNotifier

--- notice_server/notice_update_notifier.py
+++ notice_server/notice_update_notifier.py
@@ -1,3 +1,30 @@
+from enum import Enum, auto
+
+from flask_socketio import SocketIO
+
+from config import Config
+
+class NoticeUpdateType(Enum):
+    CREATED = auto()
+    REFETCHED = auto()
+    MODIFIED = auto()
+
 class NoticeUpdateNotifier:
-    def __init__(self, socketio):
-        pass
+    def __init__(self, config: Config, socketio: SocketIO) -> None:
+        self._config = config
+        self._socketio = socketio
+
+    def notify(
+            self,
+            notice_id: str,
+            update_type: NoticeUpdateType,
+            changed_data: dict
+            ) -> None:
+        self._socketio.emit(
+            event='notice_update',
+            args={
+                'notice_id': notice_id,
+                'update_type': update_type.name.lower(),
+                'changed_data': changed_data
+            }
+        )
