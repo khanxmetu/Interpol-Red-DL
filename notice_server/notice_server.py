fe1cd38c8372c4c50372f3bacf9c192880f09059
khanxmetu <khanxbahria@gmail.com>
2024-07-12 12:40:41 +0300

    dummy queue consumer

--- notice_server/notice_server.py
+++ notice_server/notice_server.py
@@ -1,7 +1,11 @@
+import time
+
 from flask import Flask
 import pika
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 NOTICES = [
     {
@@ -19,5 +23,15 @@ def notice_list_view():
 def notice_view(notice_id: str):
     pass
 
+class Consumer:
+    def __init__(self):
+        pass
+    def run(self):
+        while True:
+            socketio.emit("dummy data")
+            time.sleep(1)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    consumer = Consumer()
+    socketio.start_background_task(target=consumer.run)
+    socketio.run(app, host="0.0.0.0", debug=False, allow_unsafe_werkzeug=True)
