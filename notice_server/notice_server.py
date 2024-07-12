c5a7cfd7b46d2c4ebee2112c39722fd6b713df2b
khanxmetu <khanxbahria@gmail.com>
2024-07-12 12:16:59 +0300

    view routes

--- notice_server/notice_server.py
+++ notice_server/notice_server.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import pika
 
 app = Flask(__name__)
 
+NOTICES = [
+    {
+        "notice_1": {
+            "adas": "asdasd"
+        }
+    }
+]
+
 @app.route('/')
-def index():
-    return "hello world"
+def notice_list_view():
+    return NOTICES
+
+@app.route('/notice/<notice_id>')
+def notice_view(notice_id: str):
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
