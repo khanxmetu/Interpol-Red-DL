94391303bf97798bc1aff0c1735fe6c0dba6849d
khanxmetu <khanxbahria@gmail.com>
2024-07-15 16:36:57 +0300

    implement APIRequestSession class

---
+++ api_poller/api_poller/api_request_session.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+from api_poller.exceptions import APIRequestException
+
+
+class APIRequestSession:
+    def __init__(self, headers: dict[str, str] = {}):
+        self._session = requests.session()
+        self._session.headers = headers
+    
+    def _get_response(self, url:str) -> requests.Response:
+        response = self._session.get(url)
+        if response.status_code == 200:
+            return response
+        else:
+            raise APIRequestException(f"Received status code {response}")
+    
+    def get_json_response(self, url:str) -> dict:
+        """Retrieve json response as dict by sending a GET Request"""
+        response = self._get_response(url)
+        try:
+            return response.json()
+        except json.decoder.JSONDecodeError:
+            print(response.text)
+            raise APIRequestException(f"Expected json response")
+    
