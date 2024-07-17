21395dd9e6b53ad0df45cbc3f4caf4e6fd95adfd
khanxmetu <khanxbahria@gmail.com>
2024-07-18 00:46:41 +0300

    refactor

--- notice_server/notice_server/notice_db_manager.py
+++ notice_server/notice_server/notice_db_manager.py
@@ -2,18 +2,28 @@ import mongoengine
 from mongoengine import DoesNotExist
 
 from notice_server.models.notice import Notice
-from notice_update_notifier import NoticeUpdateType
+from notice_server.notice_update_notifier import NoticeUpdateType
 
 
 class NoticeDBManager:
     def __init__(self, db_name, db_user, db_pass, db_host, db_port) -> None:
+        self._db_name = db_name
+        self._db_user = db_user
+        self._db_pass = db_pass
+        self._db_host = db_host
+        self._db_port = db_port
+
+    def connect(self):
         mongoengine.connect(
-            db=db_name,
-            username=db_user,
-            password=db_pass,
-            host=db_host,
-            port=db_port
+            db=self._db_name,
+            username=self._db_user,
+            password=self._db_pass,
+            host=self._db_host,
+            port=self._db_port
         )
+
+    def get_all_notices(self) -> list[Notice]:
+        return Notice.objects()
 
     def get_notice_by_id(self, notice_id: str, default=None) -> Notice:
         try:
