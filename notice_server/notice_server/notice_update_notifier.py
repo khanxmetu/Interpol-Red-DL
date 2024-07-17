139fdb842fbeff494b94ef6a8ad1ffb9e0d9c159
khanxmetu <khanxbahria@gmail.com>
2024-07-17 23:37:59 +0300

    encode dates as strings

--- notice_server/notice_server/notice_update_notifier.py
+++ notice_server/notice_server/notice_update_notifier.py
@@ -1,22 +1,37 @@
 from enum import Enum, auto
+import datetime
+from werkzeug.http import http_date
 
 from flask_socketio import SocketIO
+
 
 class NoticeUpdateType(Enum):
     CREATED = auto()
     REFETCHED = auto()
     MODIFIED = auto()
 
+
 class NoticeUpdateNotifier:
     def __init__(self, socketio: SocketIO) -> None:
         self._socketio = socketio
+
+    def _prepare_notice_data_for_transmission(self, data: dict) -> None:
+        """Preprocess and updates the data to make sure dates are encodable"""
+        for key, value in data.items():
+            if (
+                isinstance(value, datetime.datetime)
+                or isinstance(value, datetime.date)
+            ):
+                data[key] = http_date(value)
 
     def notify(
             self,
             notice_id: str,
             update_type: NoticeUpdateType,
             changed_data: dict
-            ) -> None:
+    ) -> None:
+        changed_data = changed_data.copy()
+        self._prepare_notice_data_for_transmission(changed_data)
         self._socketio.emit(
             'notice_update',
             {
--- notice_server/tests/test_notice_update_notifier.py
+++ notice_server/tests/test_notice_update_notifier.py
@@ -1,6 +1,8 @@
 import pytest
 from unittest.mock import MagicMock
 from flask_socketio import SocketIO
+import datetime
+from werkzeug.http import http_date
 
 from notice_server.notice_update_notifier import NoticeUpdateNotifier, NoticeUpdateType
 
@@ -29,5 +31,38 @@ def test_notify(notice_update_notifier, mock_socketio):
             'notice_id': "1234-2020",
             'update_type': "created",
             'changed_data': {"field1": "value1", "field2": "value2"}
+        }
+    )
+
+
+def test_notify_with_notice_having_date_fields(notice_update_notifier, mock_socketio):
+    notice_id = "1234-2020"
+    update_type = MockNoticeUpdateType("CREATED")
+    cur_date = datetime.datetime(
+        2024, 7, 17, 20, 29, 49, 537770, tzinfo=datetime.timezone.utc)
+    dob = datetime.date(2020, 4, 20)
+    changed_data = {
+        "field1": "value1",
+        "date_of_birth": dob,
+        "last_fetched_date": cur_date,
+        "first_fetched_date": cur_date,
+        "last_modified_date": cur_date
+    }
+
+    notice_update_notifier.notify(notice_id, update_type, changed_data)
+
+    mock_socketio.emit.assert_called_once_with(
+        'notice_update',
+        {
+            'notice_id': "1234-2020",
+            'update_type': "created",
+            'changed_data': {
+                "field1": "value1",
+                "date_of_birth": "Mon, 20 Apr 2020 00:00:00 GMT",
+                "last_fetched_date": 'Wed, 17 Jul 2024 20:29:49 GMT',
+                "first_fetched_date": 'Wed, 17 Jul 2024 20:29:49 GMT',
+                "last_modified_date": 'Wed, 17 Jul 2024 20:29:49 GMT'
+            }
+
         }
     )
