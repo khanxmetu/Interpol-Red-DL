212ca553a61cbd217e170399eddbe18ba76b445a
khanxmetu <khanxbahria@gmail.com>
2024-07-17 19:18:11 +0300

    update field names to make it consistent with queue data

--- notice_server/models/notice.py
+++ notice_server/models/notice.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from mongoengine import Document, EmbeddedDocument, ListField
 from mongoengine import StringField, IntField, FloatField
-from mongoengine import EmbeddedDocumentListField, DateTimeField
+from mongoengine import EmbeddedDocumentListField, DateTimeField, URLField, DateField
 
 
 COUNTRIES = json.load(open('models/country_codes.json')).keys()
@@ -22,7 +22,7 @@ class ArrestWarrant(EmbeddedDocument):
 
 class Notice(Document):
     notice_id = StringField(primary_key=True, max_length=20, required=True)
-    date_of_birth = StringField(max_length=20)
+    date_of_birth = DateField(max_length=20)
     distinguishing_marks = StringField(max_length=250)
     weight = IntField(min_value=0, max_value=150)
     nationalities = ListField(StringField(choices=COUNTRIES))
@@ -36,10 +36,11 @@ class Notice(Document):
     name = StringField(max_length=100, required=True)
     languages_spoken_ids = ListField(StringField(choices=LANGUAGES))
     height = FloatField(min_value=0, max_value=2)
-    img_ids = ListField(StringField(max_length=20))
+    image_ids = ListField(StringField(max_length=20))
     last_fetched_date = DateTimeField(default=datetime.now)
     last_modified_date = DateTimeField(default=datetime.now)
     first_fetched_date = DateTimeField(default=datetime.now)
+    url = URLField()
 
     def __eq__(self, other):
         if self is other: return True
