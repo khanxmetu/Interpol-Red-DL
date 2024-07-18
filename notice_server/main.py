40e57fff9a466c3d1bdd478d8de52c29ed2ef97b
khanxmetu <khanxbahria@gmail.com>
2024-07-18 03:04:13 +0300

    setup dependencis and consumer

--- notice_server/main.py
+++ notice_server/main.py
@@ -1,10 +1,17 @@
 import os
 
 from flask import Flask, render_template, g
+from flask_socketio import SocketIO
+
 from notice_server import views
 from notice_server.notice_db_manager import NoticeDBManager
+from notice_server.notice_consumer import NoticeConsumer
+from notice_server.rabbitmq_client import RabbitMQConsumer
+from notice_server.notice_update_notifier import NoticeUpdateNotifier
 
 app = Flask("notice_server")
+socketio = SocketIO(app)
+
 
 notice_db_manager = NoticeDBManager(
     db_name=os.environ["DB_NAME"],
@@ -15,9 +22,27 @@ notice_db_manager = NoticeDBManager(
 )
 notice_db_manager.connect()
 
+rmq_consumer = RabbitMQConsumer(
+    username=os.environ["RABBITMQ_DEFAULT_USER"],
+    password=os.environ["RABBITMQ_DEFAULT_PASS"],
+    host=os.environ["RABBITMQ_HOST"],
+    port=int(os.environ["RABBITMQ_PORT"])
+)
+
+update_notifier = NoticeUpdateNotifier(socketio=socketio)
+
+notice_consumer = NoticeConsumer(
+    queue_name=os.environ["QUEUE_NAME"],
+    rmq_consumer=rmq_consumer,
+    db_manager=notice_db_manager,
+    update_notifier=update_notifier
+)
+
+
 @app.before_request
 def add_dependencies():
     g.notice_db_manager = notice_db_manager
+
 
 app.add_url_rule("/", view_func=views.index)
 app.add_url_rule("/notice_list", view_func=views.notice_list_page)
@@ -27,4 +52,5 @@ app.add_url_rule("/api/notice/<notice_id>", view_func=views.notice_detail)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    socketio.start_background_task(target=notice_consumer.run)
+    socketio.run(app, host="0.0.0.0", debug=False, allow_unsafe_werkzeug=True)
